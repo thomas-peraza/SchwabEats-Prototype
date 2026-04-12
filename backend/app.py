9a997c11b534e19819869497abd6dadef2d9af9e
thomas-peraza <txp240024@utdallas.edu>
2026-04-12 14:40:25 -0500

    Add backend setup files

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+@app.route("/")
+def home():
+    return "SchwabEats Backend Running"
+
+if __name__ == "__main__":
+    app.run(debug=True)
